f53f52eaeb17a8283d9d899b66d16ec983a04516
fumeiam <unknownajana007@gmail.com>
2025-12-24 03:45:30 +0530

    Update main.py
    1.1

--- main.py
+++ main.py
@@ -1,82 +1,96 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import JSONResponse
 
 app = FastAPI()
-
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-# ---------- SAMPLE DATA ----------
+# ---- ROLE ICON MAP (static images) ----
+ROLE_ICON = {
+    "Baron": "/static/roles/baron.png",
+    "Jungle": "/static/roles/jungle.png",
+    "Mid": "/static/roles/mid.png",
+    "Dragon": "/static/roles/dragon.png",
+    "Support": "/static/roles/support.png",
+}
+
+# ---- SAMPLE DATA (5 champs, role-specific stats) ----
 champions = [
     {
-        "id": 1,
         "name": "Ahri",
-        "role": "Mid",
-        "global_wr": 51.8,
-        "tier": "S",
-        "servers": {
-            "SEA": {"wr": 53.2, "rank1": "FoxQueen", "points": 18740},
-            "EUW": {"wr": 50.6, "rank1": "OrbMage", "points": 16210},
+        "roles": ["Mid"],
+        "icon": "/static/champions/placeholder.png",
+        "stats": {
+            "Mid": {
+                "SEA": {"wr": 53.2, "rank1": "FoxQueen", "points": 18740},
+                "EU":  {"wr": 50.6, "rank1": "OrbMage",  "points": 16210},
+                "NA":  {"wr": 49.9, "rank1": "CharmFox", "points": 15400},
+            }
         }
     },
     {
-        "id": 2,
         "name": "Kha'Zix",
-        "role": "Jungle",
-        "global_wr": 52.4,
-        "tier": "S",
-        "servers": {
-            "SEA": {"wr": 54.1, "rank1": "VoidHunter", "points": 20100},
-            "EUW": {"wr": 51.9, "rank1": "ClawEdge", "points": 17650},
+        "roles": ["Jungle"],
+        "icon": "/static/champions/placeholder.png",
+        "stats": {
+            "Jungle": {
+                "SEA": {"wr": 54.1, "rank1": "VoidHunter", "points": 20100},
+                "EU":  {"wr": 51.9, "rank1": "ClawEdge",   "points": 17650},
+                "NA":  {"wr": 50.3, "rank1": "ShadowBug",  "points": 16820},
+            }
         }
     },
     {
-        "id": 3,
         "name": "Garen",
-        "role": "Baron",
-        "global_wr": 49.7,
-        "tier": "B",
-        "servers": {
-            "SEA": {"wr": 50.1, "rank1": "DemaciaX", "points": 14300},
-            "EUW": {"wr": 49.2, "rank1": "SpinLord", "points": 13820},
+        "roles": ["Baron"],
+        "icon": "/static/champions/placeholder.png",
+        "stats": {
+            "Baron": {
+                "SEA": {"wr": 50.1, "rank1": "DemaciaX", "points": 14300},
+                "EU":  {"wr": 49.2, "rank1": "SpinLord", "points": 13820},
+                "NA":  {"wr": 48.7, "rank1": "JusticeMan","points": 13210},
+            }
         }
     },
     {
-        "id": 4,
         "name": "Lucian",
-        "role": "Dragon",
-        "global_wr": 50.9,
-        "tier": "A",
-        "servers": {
-            "SEA": {"wr": 51.7, "rank1": "DualShot", "points": 16980},
-            "EUW": {"wr": 50.1, "rank1": "Relentless", "points": 15800},
+        "roles": ["Dragon", "Mid"],  # primary = Dragon
+        "icon": "/static/champions/placeholder.png",
+        "stats": {
+            "Dragon": {
+                "SEA": {"wr": 51.7, "rank1": "DualShot",  "points": 16980},
+                "EU":  {"wr": 50.1, "rank1": "Relentless","points": 15800},
+                "NA":  {"wr": 49.6, "rank1": "TwinPistol","points": 14950},
+            },
+            "Mid": {
+                "SEA": {"wr": 50.2, "rank1": "LaneKing",  "points": 16010},
+                "EU":  {"wr": 49.4, "rank1": "SideStep",  "points": 15030},
+                "NA":  {"wr": 48.9, "rank1": "QuickDraw","points": 14420},
+            }
         }
     },
     {
-        "id": 5,
         "name": "Blitzcrank",
-        "role": "Support",
-        "global_wr": 48.6,
-        "tier": "C",
-        "servers": {
-            "SEA": {"wr": 49.4, "rank1": "HookGod", "points": 13210},
-            "EUW": {"wr": 47.8, "rank1": "IronFist", "points": 12400},
+        "roles": ["Support"],
+        "icon": "/static/champions/placeholder.png",
+        "stats": {
+            "Support": {
+                "SEA": {"wr": 49.4, "rank1": "HookGod",  "points": 13210},
+                "EU":  {"wr": 47.8, "rank1": "IronFist","points": 12400},
+                "NA":  {"wr": 47.2, "rank1": "GrabBot","points": 11980},
+            }
         }
-    }
+    },
 ]
 
 @app.get("/")
 def champions_page(request: Request):
     return templates.TemplateResponse(
         "champions.html",
-        {"request": request, "champions": champions}
-    )
-
-@app.get("/tierlist")
-def tierlist_page(request: Request):
-    return templates.TemplateResponse(
-        "tierlist.html",
-        {"request": request, "champions": champions}
+        {
+            "request": request,
+            "champions": champions,
+            "role_icon": ROLE_ICON
+        }
     )
